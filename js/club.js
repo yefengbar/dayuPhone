afb622b18706ebd5b457cf54af42c3dbb7621f15
yefengbar <718063475@qq.com>
2016-10-19 17:37:26 +0800

    is done

---
+++ js/club.js
@@ -0,0 +1,212 @@
+var clubTxt = {
+	'anhui':{
+		"tit":'安徽仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/palanhui/home</td></tr>'+
+			'<tr><td class="con_t">群　　号：</td><td>62389239</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>回梦一曲柔肠断<br>徽风挽剑续仙缘</td></tr>'+
+		'</table>'
+	},
+	'beijing':{
+		"tit":'北京仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/yijianxuan</td></tr>'+
+			'<tr><td class="con_t">群　　号：</td><td>1147090</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>逸尘逍遥傲云天<br>剑歌峥嵘诉流年<br>轩昂侠义应犹在<br>同处此间是为缘</td></tr>'+
+		'</table>'
+	},
+	'chongqing':{
+		"tit":'重庆仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/u/2617998397</td></tr>'+
+			'<tr><td class="con_t">群　　号：</td><td>52777797</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>渝州奇侠，感动长行</td></tr>'+
+		'</table>'
+	},
+	'dongbei':{
+		"tit":'东北仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/xianjiandbhyh</td></tr>'+
+			'<tr><td class="con_t">群　　号1：</td><td>60272869</td></tr>'+
+			'<tr><td class="con_t">群　　号2：</td><td>194379075</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>重走蜀山路，再续仙缘情，<br>东北仙剑后援会期待您的加入。</td></tr>'+
+		'</table>'
+	},
+	'fujian':{
+		"tit":'福建仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/p/1005052184018560</td></tr>'+
+			'<tr><td class="con_t">群　　号1：</td><td>106302511</td></tr>'+
+			'<tr><td class="con_t">群　　号2：</td><td>107813167</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>仙灵缘起逝水痕，<br>剑意流光梦前尘。<br>奇缘难断多少事，<br>侠隐归尘终无期。<br>我只在乎你，<br>仙剑因你而感动。</td></tr>'+
+		'</table>'
+	},
+	'guangdong':{
+		"tit":'广东仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/xianmihouyuanhui</td></tr>'+
+			'<tr><td class="con_t">群　　号1：</td><td>118174021</td></tr>'+
+			'<tr><td class="con_t">群　　号2：</td><td>132422906</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>铸仙剑之魂，绘粤剑之图</td></tr>'+
+		'</table>'
+	},
+	'guangxi':{
+		"tit":'广西仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/23040484657</td></tr>'+
+			'<tr><td class="con_t">群　　号1：</td><td>121197194</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>从来不需要想起，<br>永远也不用忘记。<br>相聚，若有缘，<br>仙剑因你我而感动。</td></tr>'+
+		'</table>'
+	},
+	'guizhou':{
+		"tit":'贵州仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/likeufo</td></tr>'+
+			'<tr><td class="con_t">群　　号：</td><td>134684033</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>仙剑奇侠传贵州后援会,希望大家多多支持</td></tr>'+
+		'</table>'
+	},
+	'hainan':{
+		"tit":'海南仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/xj5963207</td></tr>'+
+			'<tr><td class="con_t">群　　号1：</td><td>5963207</td></tr>'+
+			'<tr><td class="con_t">群　　号2：</td><td>260496145</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>天海之南，仙道无涯。<br>剑之牵绊，知道永远。</td></tr>'+
+		'</table>'
+	},
+	'hebei':{
+		"tit":'河北仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/yizhilanlaosu</td></tr>'+
+			'<tr><td class="con_t">群　　号：</td><td>33230936</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>让更多的仙剑迷聚集在一起创造一个良好的交流环境。天下仙迷一家亲。</td></tr>'+
+		'</table>'
+	},
+	'henan':{
+		"tit":'河南仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/palhnhyh</td></tr>'+
+			'<tr><td class="con_t">群　　号：</td><td>101597522</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>十年一觉仙入梦<br>百载独醒剑出凡</td></tr>'+
+		'</table>'
+	},
+	'hubei':{
+		"tit":'湖北仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/u/2617998397</td></tr>'+
+			'<tr><td class="con_t">群　　号1：</td><td>152695161</td></tr>'+
+			'<tr><td class="con_t">群　　号2：</td><td>34672221</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>千湖之省藏仙音，荆楚之城飞剑影，仙剑湖北后援会,<br>欢迎各位仙迷加入</td></tr>'+
+		'</table>'
+	},
+	'hunan':{
+		"tit":'湖南仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/palhn</td></tr>'+
+			'<tr><td class="con_t">群　　号：</td><td>157241385</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>岳麓藏仙踪，洞庭孕剑魂。<br>数侠骨柔情，还看潇湘。</td></tr>'+
+		'</table>'
+	},
+	'jiangsu':{
+		"tit":'江苏仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/jspal</td></tr>'+
+			'<tr><td class="con_t">群　　号1：</td><td>150644624</td></tr>'+
+			'<tr><td class="con_t">群　　号2：</td><td>126914688</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>云舒雨霁心愿了，<br>御剑江湖总相随！<br>这里是江苏仙剑后援会,<br>欢迎各位籼米~</td></tr>'+
+		'</table>'
+	},
+	'jiangxi':{
+		"tit":'江西仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/xjjxhyh</td></tr>'+
+			'<tr><td class="con_t">群　　号：</td><td>159231609</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>仗剑江湖梦已远，<br>仙剑奇侠永流传！<br>守候二十年的感动，<br>延续心中的那一份美好！<br>和江西的仙迷一起守候仙剑，<br>守护仙剑！江西仙剑后援会，<br>因你的加入而精彩！</td></tr>'+
+		'</table>'
+	},
+	'neimenggu':{
+		"tit":'内蒙古仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/mssdn</td></tr>'+
+			'<tr><td class="con_t">群　　号：</td><td>129730958</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>逸尘逍遥傲云天<br>剑歌峥嵘诉流年<br>轩昂侠义应犹在<br>同处此间是为缘</td></tr>'+
+		'</table>'
+	},
+	'sanxi':{
+		"tit":'陕西仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/qinpal</td></tr>'+
+			'<tr><td class="con_t">群　　号：</td><td>113873795</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>秦人秦声三秦地，相亲相爱仙剑情！陕西，在这个拥有三十朝历史的大地上拥有一群热爱仙剑的人。如果你也热爱仙剑，欢迎加入我们,这里有志同道合的伙伴,相信这里是你最温暖的家！</td></tr>'+
+		'</table>'
+	},
+	'shandong':{
+		"tit":'山东仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/2281011745</td></tr>'+
+			'<tr><td class="con_t">群　　号：</td><td>56627643</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>浓浓仙剑情，<br>璀璨即墨烟花灯；<br>时时觅仙友，<br>相约齐鲁仗剑行</td></tr>'+
+		'</table>'
+	},
+	'shanghai':{
+		"tit":'上海仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/palsh</td></tr>'+
+			'<tr><td class="con_t">群　　号：</td><td>2149673</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>仙剑情结，生生世世！<br>仙剑出到老，我们玩到老</td></tr>'+
+		'</table>'
+	},
+	'shanxi':{
+		"tit":'山西仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/mengpy</td></tr>'+
+			'<tr><td class="con_t">群　　号：</td><td>69945871</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>相逢是缘，仙剑永恒。</td></tr>'+
+		'</table>'
+	},
+	'sichuan':{
+		"tit":'四川仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/1352349403</td></tr>'+
+			'<tr><td class="con_t">群　　号1：</td><td>146719872</td></tr>'+
+			'<tr><td class="con_t">群　　号2：</td><td>155703273</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>五灵锁妖，剑翔九州。<br>重生，等待，翻开轮回的卷轴<br>我们在蜀巅相遇。</td></tr>'+
+		'</table>'
+	},
+	'tianjin':{
+		"tit":'天津仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/tjzxg</td></tr>'+
+			'<tr><td class="con_t">群　　号1：</td><td>160735193</td></tr>'+
+			'<tr><td class="con_t">群　　号2：</td><td>178809252</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>天子渡口，前缘津续。</td></tr>'+
+		'</table>'
+	},
+	'xibei':{
+		"tit":'西北仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/2361702814</td></tr>'+
+			'<tr><td class="con_t">群　　号：</td><td>142682346</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>仙灵尘世远，剑舞天地间。<br>望西疆路遥，思北荒峰寒。<br>千杯后知己，万里援红颜。<br>问君何时归？心愿自始还。</td></tr>'+
+		'</table>'
+	},
+	'yunnan':{
+		"tit":'云南仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/2529218320</td></tr>'+
+			'<tr><td class="con_t">群　　号：</td><td>182435454</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>云起梦缘，只因有你。<br>云南仙剑，因你而在</td></tr>'+
+		'</table>'
+	},
+	'zhejiang':{
+		"tit":'浙江仙剑后援会',
+		"con":'<table>'+
+			'<tr><td class="con_t">新浪微博：</td><td>weibo.com/xjzjhy</td></tr>'+
+			'<tr><td class="con_t">群　　号1：</td><td>156018260</td></tr>'+
+			'<tr><td class="con_t">群　　号2：</td><td>163763649</td></tr>'+
+			'<tr><td class="con_t">宣  传  语：</td><td>往事如烟，魂萦梦牵，<br>增添我心中的思念；<br>纵然追寻万年，<br>今生的情缘不变。<br>仙剑并不孤单，<br>仙剑因你而精彩。</td></tr>'+
+		'</table>'
+	}
+}
+
